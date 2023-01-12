26bbc657553960984ebc9c19e4a53bd395fdc325
NaneKarapetyan304 <nanekarapetyann304@gmail.com>
2023-01-12 11:26:07 +0400

    tasks

--- ArrayMethods/map.js
+++ ArrayMethods/map.js
@@ -9,7 +9,6 @@ function doubleNumbers(arr){
 
   //2) Take an array of numbers and make them strings
     function stringItUp(arr1){
-    // your code here
         return arr1.map(function(item){
             item = String(item);
             return item
@@ -29,7 +28,6 @@ function doubleNumbers(arr){
 
 //4)Make an array of strings of the names
   function namesOnly(arr){
-    // your code here
       return arr.map(function(item){
           return item.name
       })
@@ -60,7 +58,6 @@ function doubleNumbers(arr){
 
   //5) Make an array of strings of the names saying whether or not they can go to The Matrix
   function makeStrings(arr){
-    // your code here
       return arr.map(function(item){
           if(item.age >= 18){
               return `${item.name} can go to The Matrix`
@@ -91,3 +88,41 @@ function doubleNumbers(arr){
           age: 100
       }
   ])); 
+  //6) Make an array of the names in h1s, and the ages in h2s
+function readyToPutInTheDOM(arr){
+    return arr.map(function(item){
+        return `<h1>${item.name}</h1><h2>${item.age}</h2> `
+    });
+    }
+   console.log(readyToPutInTheDOM([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+])); 
+
+//7)Square the value of every element in the array. Presume that you will only get numbers in the input array.
+
+function SquareValue(arr){
+    return arr.map(function(num){
+        return Math.pow(num,2);
+    });
+};
+console.log(SquareValue([1, 2, 3, 4, 5]));
+
